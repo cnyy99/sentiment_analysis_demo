af673abdf2f020d1acb2604351aabb61df09f207
cnyy <chennansoul@gmail.com>
2019-04-16 18:21:50 +0800

    change jsp to the thymeleaf

--- src/main/java/com/bjfu/sentimentanalysis/demo/controller/IndexController.java
+++ src/main/java/com/bjfu/sentimentanalysis/demo/controller/IndexController.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class IndexController {
